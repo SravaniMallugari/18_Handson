15ae4275bfc1ec17841009a7eb4a3e09608e2b2c
SravaniMallugari <111357901+SravaniMallugari@users.noreply.github.com>
2022-08-21 10:49:13 +0530

    qstn3

--- Program.cs
+++ Program.cs
@@ -4,17 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace qstn2
+namespace qstn3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int num1, num2, num3;
-            Console.WriteLine(Convert.ToInt32("input enter first num1" + num1));
-            Console.WriteLine(Convert.ToInt32("input enter first num2" + num2));
-            Console.WriteLine(Convert.ToInt32("input enter first num3" + num3));
-            Console.WriteLine(Convert.ToInt32("output{0} * {1} *{2} "  num1 * num2 * num3));
+            Console.Write("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter another number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("{0} + {1} = {2}", num1, num2, num1 + num2);
+            Console.WriteLine("{0} - {1} = {2}", num1, num2, num1 - num2);
+            Console.WriteLine("{0} x {1} = {2}", num1, num2, num1 * num2);
+            Console.WriteLine("{0} / {1} = {2}", num1, num2, num1 / num2);
+            Console.WriteLine("{0} mod {1} = {2}", num1, num2, num1 % num2);
             Console.ReadLine();
         }
     }
